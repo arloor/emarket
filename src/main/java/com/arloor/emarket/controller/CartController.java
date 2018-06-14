a780d98900e2ab8f83afadd8b2ab477355d3dc0b
arloor <admin@arloor.com>
2018-06-14 17:45:13 +0800

    设置cart的redis记录的超时时间为100

--- src/main/java/com/arloor/emarket/controller/CartController.java
+++ src/main/java/com/arloor/emarket/controller/CartController.java
@@ -1,5 +1,7 @@
 package com.arloor.emarket.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
+
+    Logger logger=LoggerFactory.getLogger(CartController.class);
     @Autowired
     RedisTemplate redisTemplate;
 
@@ -23,6 +29,7 @@ public class CartController {
     @RequestMapping(value = "/updateCart",produces = "application/json")
     public  void updateCartByUname(@RequestParam String uname,@RequestParam String cart){
         ValueOperations valueOps=redisTemplate.opsForValue();
-        valueOps.set("cart:"+uname,cart);
+        valueOps.set("cart:"+uname,cart,60*10,TimeUnit.SECONDS);
+        logger.info("cart:"+uname+" redis记录设置成功，超时时间："+redisTemplate.getExpire("cart:"+uname)+" seconds");
     }
 }
